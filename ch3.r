18de2078f5c64d85b4172e8ea5df5a746476a6df
DataEconomyLab <dataeconomylab@gmail.com>
2021-03-23 15:51:25 +0900

    Create ch3.r

---
+++ ch3.r
@@ -0,0 +1,41 @@
+hospital = read.table("C:/work/data/reg2020/hospital.txt", header=T)
+head(hospital)
+
+hospital.lm = lm(Y ~ X1 + X2 + X3 + X4 + X5, data=hospital)
+summary(hospital.lm)
+anova(hospital.lm)
+
+install.packages("fmsb")
+library(fmsb)
+VIF(lm(X2 ~ X1+X3+X4+X5, data=hospital))
+VIF(lm(X3 ~ X1+X2+X4+X5, data=hospital))
+VIF(lm(X4 ~ X1+X2+X3+X5, data=hospital))
+VIF(lm(X5 ~ X1+X2+X3+X4, data=hospital))
+
+cor(hospital[,-6])
+summary(lm(Y ~ X2 + X3 + X4 + X5, data=hospital))
+
+VIF(lm(X2 ~ X3+X4+X5, data=hospital))
+VIF(lm(X3 ~ X2+X4+X5, data=hospital))
+VIF(lm(X4 ~ X2+X3+X5, data=hospital))
+VIF(lm(X5 ~ X2+X3+X4, data=hospital))
+
+hald = read.table("C:/work/data/reg2020/hald.txt", header=T)
+head(hald)
+
+install.packages("leaps")
+library(leaps)
+all.lm = regsubsets(Y ~., data=hald)
+(rs = summary(all.lm))
+
+names(rs)
+rs$rsq
+rs$adjr2
+rs$cp
+
+start.lm = lm(Y~1, data=hald)
+full.lm = lm(Y~., data=hald)
+
+step(start.lm, scope=list(lower=start.lm, upper=full.lm), direction="forward")
+step(full.lm, data=hald, direction="backward")
+step(start.lm, scope=list(upper=full.lm), data=hald, direction="both")
