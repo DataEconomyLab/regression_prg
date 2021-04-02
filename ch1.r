8b332e28e370a76147246c3b97fee57c9b838446
DataEconomyLab <dataeconomylab@gmail.com>
2021-04-02 17:12:24 +0900

    Update ch1.r

--- ch1.r
+++ ch1.r
@@ -1,54 +1,65 @@
-market = read.table("C:/work/data/reg2020/market-1.txt", header=TRUE)
+market = read.table("https://raw.githubusercontent.com/DataEconomyLab/regression/main/reg2020/market-1.txt", header=TRUE)
 head(market)
 
 market.lm = lm(Y ~ X, data=market)
 summary(market.lm)
 
-plot(market$X, market$Y, xlab="±¤°í·á", ylab="ÃÑÆÇ¸Å¾×", pch=19)
-title("±¤°í·á¿Í ÆÇ¸Å¾×ÀÇ »êÁ¡µµ")
+plot(market$X, market$Y, xlab="ê´‘ê³ ë£Œ", ylab="ì´íŒë§¤ì•¡", pch=19)
+title("ê´‘ê³ ë£Œì™€ íŒë§¤ì•¡ì˜ ì‚°ì ë„")
+
+market.lm = lm(Y ~ X, data=market)
+summary(market.lm)
+
+plot(market$X, market$Y, xlab="ê´‘ê³ ë£Œ", ylab="ì´íŒë§¤ì•¡", pch=19)
+title("ê´‘ê³ ë£Œì™€ íŒë§¤ì•¡ì˜ ì‚°ì ë„")
 abline(market.lm)
 
 names(market.lm)
+
 market.lm$resid
+
 sum(market.lm$resid)
-sum(market$X * market.lm$resid)
+
+sum(market$X*market.lm$resid)
 
 anova(market.lm)
 
 qf(0.95, 1, 13)
-1-pf(192.9,1,13)
+
+1 - pf(192.9, 1, 13)
 
 summary(market.lm)
 
-pred.frame = data.frame(X=seq(3.5, 14.5, 0.2))
+pred.frame=data.frame(X=seq(3.5, 14.5, 0.2))
 pc = predict(market.lm, int="c", newdata=pred.frame)
-pp = predict(market.lm, int="p", newdata=pred.frame)
+pp=predict(market.lm, int="p", newdata=pred.frame)
 head(pc)
-head(pp)
 
 pc = predict(market.lm, int="c", newdata=pred.frame)
+
 pp = predict(market.lm, int="p", newdata=pred.frame)
 
 pred.X = pred.frame$X
 pred.X
 
 plot(market$X, market$Y, ylim=range(market$Y, pp))
-matlines(pred.X, pc, lty=c(1,2,3), col="BLUE")
+matlines(pred.X, pc, lty=c(1,2,2), col="BLUE")
 matlines(pred.X, pp, lty=c(1,3,3), col="RED")
 
 summary(market.lm)
+
 2*(1-pt(13.889, 13))
 
-x = c(1,2,3,4,5)
-y = c(2,3,5,8,7)
+x = c(1, 2, 3, 4, 5)
+y = c(2, 3, 5, 8, 7)
 w = 1/x
 w.lm = lm(y ~ x, weights=w)
 summary(w.lm)
 
 anova(w.lm)
 
-super = read.table("C:/work/data/reg2020/supermarket.txt", header=T)
-head(super,3)
+super = read.table("https://raw.githubusercontent.com/DataEconomyLab/regression/main/reg2020/supermarket.txt", header=T)
+head(super, 3)
 
 attach(super)
 plot(price, time, pch=19)
@@ -57,14 +68,15 @@ super.lm = lm(time ~ price, data=super)
 summary(super.lm)
 
 anova(super.lm)
-names(super.lm)
 
+names(super.lm)
 cbind(super, super.lm$resid, super.lm$fitted)
+
 plot(price, super.lm$resid, pch=19)
 abline(h=0, lty=2)
 
-p.x = data.frame(price=c(1,45))
+p.x = data.frame(price=c(1, 45))
 pc = predict(super.lm, int="c", newdata=p.x)
 pred.x = p.x$price
-plot(super$price, super$time, ylim=range(super$time, pc))
-matlines(pred.x, pc, lty=c(1,2,2), col="blue")
+plot(super$price, super$time, ylim = range(super$time, pc))
+matlines(pred.x, pc, lty=c(1,2,3), col="BLUE")
